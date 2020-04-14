43e6ad96aaa8f30086b2df9e9262b8f98c39afe4
David Spector <davidspector08@gmail.com>
2020-04-14 14:42:20 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,7 +2,9 @@
 require 'pry'
 
 def game_hash
-  {home = {:team_name => "Brooklyn Nets", :colors => ["Black", "White"], :players => [
+  game_hash = 
+  {
+    home = {:team_name => "Brooklyn Nets", :colors => ["Black", "White"], :players => [
    { player_name: 'Alan Anderson',
           number: 0,
           shoe: 16,
